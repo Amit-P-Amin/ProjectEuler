3d7fa29f3ae94b52573efdfb1ca92a5f85ac8784
Amit Amin <amitpamin@gmail.com>
2015-03-04 13:35:01 -0800

    Four more done again

--- lib/11-20/14.rb
+++ lib/11-20/14.rb
@@ -1,18 +1,44 @@
-# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# The following iterative sequence is defined for the set of positive integers:
 #
-# Find the largest palindrome made from the product of two 3-digit numbers.
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+#
+# Using the rule above and starting with 13, we generate the following sequence:
+#
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+#
+# Which starting number, under one million, produces the longest chain?
+#
+# NOTE: Once the chain starts the terms are allowed to go above one million.
 
 require_relative '../../utility_functions'
 
-def products(digits)
-  numbers = []
-  (10**(digits-1)...10**(digits)).each do |num1|
-    (10**(digits-1)...10**(digits)).each do |num2|
-      numbers << num1 * num2
+def collatz_terms(num)
+  count = 1
+  while num != 1
+    if num.even?
+      num = num / 2
+    else
+      num = num * 3 + 1
+    end
+    count += 1
+  end
+
+  count
+end
+
+def longest_terms(limit)
+  longest = 0
+  best_num = 0
+  (1..limit).each do |num|
+    if collatz_terms(num) > longest
+      longest = collatz_terms(num)
+      best_num = num
     end
   end
 
-  numbers.uniq
+  best_num
 end
 
-puts products(3).select {|n| n.palindrome?}.sort.last
+p longest_terms(1000000)
